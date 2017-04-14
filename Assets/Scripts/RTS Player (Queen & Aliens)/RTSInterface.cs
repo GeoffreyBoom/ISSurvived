58ddc7ea7f45856da34d5c4e10fde723c4a03427
DESKTOP-PBFPIT1\olyve <olyver.cabana@live.com>
2017-04-14 11:02:35 -0400

    commented and edited enemyBehaviour and RTSInterface

--- Assets/Scripts/RTS Player (Queen & Aliens)/RTSInterface.cs
+++ Assets/Scripts/RTS Player (Queen & Aliens)/RTSInterface.cs
@@ -3,52 +3,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-
 public class RTSInterface : MonoBehaviour
 {
-    [SerializeField]
-    FlockBehaviour flockPrefab;
-
+    
     List<EnemyBehaviour> enemies;
-
-    List<EnemyBehaviour> newFlock;
-    List<FlockBehaviour> flocks;
-
     Camera_RTS cam;
-
     bool patrol = false;
-    bool multipleEnemiesSelected;
-
     int index = 0;
-
     string moveOrPatrolString = "MOVE";
-
-    // EnemyBehaviour currentlyControlledEnemy;
+    EnemyBehaviour currentlyControlledEnemy;
     Vector3 target;
+
     // Use this for initialization
     void Start()
     {
+        //Initialize the num of resources
         Text crystalText = this.GetComponentsInChildren<Text>()[0];
         int resNum = int.Parse(crystalText.text) + 10;
         string temp = resNum + "";
         crystalText.text = temp;
-
-        enemies = new List<EnemyBehaviour>();
-        enemies.AddRange(FindObjectsOfType<EnemyBehaviour>());
-        flocks = new List<FlockBehaviour>();
-        newFlock = new List<EnemyBehaviour>();
+        //add a reference to the camera
         cam = FindObjectOfType<Camera_RTS>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        refreshEnemyList();
         getKeyboardInputs();
         getMouseInputs();
 
     }
+
+    void refreshEnemyList()
+    {
+        //get the list of npcs
+        enemies = new List<EnemyBehaviour>();
+        enemies.AddRange(FindObjectsOfType<EnemyBehaviour>());
+    }
+
     void getKeyboardInputs()
     {
+        //check for keyboard inputs
         if (Input.GetKeyDown(KeyCode.P))
         {
             MoveOrPatrolBtnClicked();
@@ -57,192 +53,82 @@ public class RTSInterface : MonoBehaviour
         {
             FindNextClicked();
         }
-
         if (Input.GetKeyDown(KeyCode.I))
         {
-            if (multipleEnemiesSelected)
-            {
-                createNewFlock();
-                foreach (EnemyBehaviour en in flocks[flocks.Count - 1].enemies)
-                {
-                    en.clearAllFutur();
-                    en.currentState = EnemyBehaviour.State.Idle;
-                }
-
-                /*else if (currentlyControlledEnemy != null)
-                {
-                    currentlyControlledEnemy.clearAllFutur();
-                    currentlyControlledEnemy.currentState = EnemyBehaviour.State.Idle;
-                }*/
-            }
-            /* else
-             {
-                 if (currentlyControlledEnemy != null)
-                 {
-                     currentlyControlledEnemy.clearAllFutur();
-                     currentlyControlledEnemy.currentState = EnemyBehaviour.State.Idle;
-                 }
-             }*/
-        }
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            multipleEnemiesSelected = true;
-            //currentlyControlledEnemy = null;
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftControl))
-        {
-            multipleEnemiesSelected = false;
+            //the npc goes to its idle state
+            currentlyControlledEnemy.clearAllFutur();
+            currentlyControlledEnemy.currentState = EnemyBehaviour.State.Idle;
         }
     }
-
-    void createNewFlock()
-    {
-        if (newFlock.Count > 0)
-        {
-            foreach (FlockBehaviour fl in flocks)
-            {
-                foreach (EnemyBehaviour en in newFlock)
-                {
-                    fl.enemies.Remove(en);
-                }
-            }
-            for (int i = 0; i < flocks.Count; i++)
-            {
-                if (flocks[i].enemies.Count < 1)
-                {
-                    FlockBehaviour temp = flocks[i];
-                    flocks.RemoveAt(i);
-                    Destroy(temp);
-
-                }
-            }
-            //FlockBehaviour flock = new FlockBehaviour();
-            FlockBehaviour tempFlock = Instantiate(flockPrefab, Vector3.zero, Quaternion.identity) as FlockBehaviour;
-            flocks.Add(tempFlock);
-            foreach (EnemyBehaviour en in newFlock)
-            {
-                flocks[flocks.Count - 1].enemies.Add(en);
-            }
-            Debug.Log("Aliens in flock: " + flocks[flocks.Count - 1].enemies.Count);
-        }/*else if (newFlock.Count == 1)
-        {
-            multipleEnemiesSelected = false;
-            currentlyControlledEnemy = newFlock[0];
-        }*/
-        newFlock = new List<EnemyBehaviour>();
-    }
-
     void getMouseInputs()
     {
+        //check for mouse inputs
         if (Input.GetMouseButtonDown(1))
         {
-            //currentlyControlledEnemy = null;
-
-            createNewFlock();
+            //deselect current npc
+            currentlyControlledEnemy = null;
         }
         if (Input.GetMouseButtonDown(0))
         {
+            //do a raycast to see what was hit
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
             {
-
                 if (hit.collider.gameObject.tag.Equals("Alien"))
                 {
-                    if (multipleEnemiesSelected)
-                    {
-                        newFlock.Add(hit.collider.GetComponentInParent<EnemyBehaviour>());
-                    }
-                    else
-                    {
-                        //wasClickedOn(hit.collider.GetComponentInParent<EnemyBehaviour>());
-                        createNewFlock();
-                        newFlock.Add(hit.collider.GetComponentInParent<EnemyBehaviour>());
-                    }
-
+                    //try selecting npc
+                    wasClickedOn(hit.collider.GetComponentInParent<EnemyBehaviour>());
                 }
                 else if (hit.collider.gameObject.tag == "Floor")
                 {
-
+                    //try giving currently controlled npc a command
                     target = new Vector3(hit.point.x, hit.point.y, hit.point.z);
-                    //if (multipleEnemiesSelected)
-                    // {
-                    // createNewFlock();
-                    // if (multipleEnemiesSelected )
-                    // {
-                    giveCommandToFlock(flocks.Count - 1);
-                    //}
-                    // else
-                    // {
-                    //     giveCommand();
-                    //  }
-                    //  }
-                    // else
-                    // {
-                    //      giveCommand();
-                    // }
+                    giveCommand();
                 }
             }
         }
     }
-    /*
+
     public void wasClickedOn(EnemyBehaviour e)
     {
-        Debug.Log("Has e");
-        currentlyControlledEnemy = e;
-    }
-    */
-    /* void giveCommand()
-     {
-         if(currentlyControlledEnemy != null)
-         {
-             currentlyControlledEnemy.clearAllFutur();
-             Debug.Log(target);
-             if (patrol)
-             {
-                 Debug.Log("Patrol");
-                 currentlyControlledEnemy.currentState = EnemyBehaviour.State.Patrol;
-                 currentlyControlledEnemy.assignTarget(currentlyControlledEnemy.transform.position);
-                 currentlyControlledEnemy.assignPatrolTarget(target);
-             }
-             else
-             {
-                 Debug.Log("Move To");
-                 currentlyControlledEnemy.currentState = EnemyBehaviour.State.MoveTo;
-                 currentlyControlledEnemy.assignTarget(target);
-             }
-         }
-     }*/
-
-    void giveCommandToFlock(int index)
-    {
-        if (index < flocks.Count && index >= 0)
+        //if npc was already selected, deselect, else select
+        if (currentlyControlledEnemy.Equals(e))
         {
-            foreach (EnemyBehaviour en in flocks[index].enemies)
+            currentlyControlledEnemy = null;
+        }else
+        {
+            currentlyControlledEnemy = e;
+        }        
+    }
+
+    void giveCommand()
+    {
+        //if an npc is selected
+        if (currentlyControlledEnemy != null)
+        {
+            currentlyControlledEnemy.clearAllFutur();
+            //if the npc is told to patrol
+            if (patrol)
             {
-                en.clearAllFutur();
-                Debug.Log(target);
-                if (patrol)
-                {
-                    Debug.Log("Patrol");
-                    en.currentState = EnemyBehaviour.State.Patrol;
-                    en.assignTarget(en.transform.position);
-                    en.assignPatrolTarget(target);
-                }
-                else
-                {
-                    Debug.Log("Move To");
-                    en.currentState = EnemyBehaviour.State.MoveTo;
-                    en.assignTarget(target);
-                }
+                currentlyControlledEnemy.currentState = EnemyBehaviour.State.Patrol;
+                currentlyControlledEnemy.assignTarget(currentlyControlledEnemy.transform.position);
+                currentlyControlledEnemy.assignPatrolTarget(target);
+            }
+            else
+            {
+                //else it moves to the designated position
+                currentlyControlledEnemy.currentState = EnemyBehaviour.State.MoveTo;
+                currentlyControlledEnemy.assignTarget(target);
             }
         }
     }
 
     public void MoveOrPatrolBtnClicked()
     {
+        //changes whether next command will be to patrol or to move
         patrol = !patrol;
-        Text mpText = this.GetComponentsInChildren<Text>()[2];
+        Text mpText = this.GetComponentsInChildren<Text>()[1];
         string temp = mpText.text;
         mpText.text = moveOrPatrolString;
         moveOrPatrolString = temp;
@@ -251,13 +137,14 @@ public class RTSInterface : MonoBehaviour
 
     public void FindNextClicked()
     {
+        //searches the list of enemy for the next enemy that is in its idle state
         int prevIndex = index;
-
         while ((++index) % enemies.Count != prevIndex)
         {
             index = index % enemies.Count;
             if (enemies[index].currentState == EnemyBehaviour.State.Idle)
             {
+                //moves the camera over the npc
                 cam.changeCameraTarget(enemies[index].gameObject);
                 break;
             }
@@ -266,7 +153,8 @@ public class RTSInterface : MonoBehaviour
 
     public void GotResource()
     {
-        Text crystalText = this.GetComponentsInChildren<Text>()[1];
+        //updates the number of resources
+        Text crystalText = this.GetComponentsInChildren<Text>()[0];
         int resNum = int.Parse(crystalText.text) + 1;
         string temp = resNum + "";
         crystalText.text = temp;
@@ -274,10 +162,12 @@ public class RTSInterface : MonoBehaviour
 
     public bool trySpawningEggs()
     {
+        //checks whether or not there are enough resources to spawn an egg
         Text crystalText = this.GetComponentsInChildren<Text>()[0];
         int resNum = int.Parse(crystalText.text);
         if (resNum >= 2)
         {
+            //if there is, update the number of resources
             resNum -= 2;
             string temp = resNum + "";
             crystalText.text = temp;
